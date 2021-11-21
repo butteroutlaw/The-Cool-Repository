b78f7ad207d8f8ea6f9f917d2c2687a2fe49e633
sahire2004 <94800260+sahire2004@users.noreply.github.com>
2021-11-21 13:44:08 -0600

    Add files via upload

---
+++ login.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+
+public class login {
+	public static void main(String[] args) {
+		HashMap<String, String> map = signup.getMap();
+		HashMap<String, String> q1 = signup.getq1();
+		HashMap<String, String> q2 = signup.getq2();
+		HashMap<String, String> q3 = signup.getq3();
+		HashMap<String, String> q4 = signup.getq4();
+		HashMap<String, String> q5 = signup.getq5();
+		HashMap<String, String> q6 = signup.getq6();
+		HashMap<String, String> q7 = signup.getq7();
+	}
+}
